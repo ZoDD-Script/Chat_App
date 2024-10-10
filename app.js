47fd1bbfb2fe2557ac48e02afdb4ee35563a0caa
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-10-10 19:23:47 +0100

    feat: loading old chats between users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 const userRoute = require('./routes/userRoute');
 const User = require('./models/userModel');
+const Chat = require('./models/chatModel');
 
 // Create an instance of Express
 const app = express();
@@ -44,6 +45,16 @@ usp.on('connection', async function(socket) {
   //chattig implementation
   socket.on('newChat', function(data) {
     socket.broadcast.emit('loadNewChat', data);
+  });
+
+  // load old chats
+  socket.on('existsChat', async function(data) {
+    let chats = await Chat.find({ $or: [
+      { sender_id: data.sender_id, receiver_id: data.receiver_id },
+      { sender_id: data.receiver_id, receiver_id: data.sender_id },
+    ] });
+
+    socket.emit('loadChats', { chats: chats });
   });
 });
 
